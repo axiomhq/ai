7127277abfb9db1812b77d34d729cd979904263e
Islam Shehata <islam@axiom.co>
2025-06-10 13:54:23 +0400

    config internal eslint package

--- packages/cli/vite.config.ts
+++ packages/cli/vite.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-    plugins: [
-        dts({
-            outDir: 'dist/types'
-        })
-    ],
-    build: {
-        target: 'node22',
-        lib: {
-            entry: resolve(__dirname, 'src/index.ts'),
-            formats: ['es'],
-            fileName: () => 'index.js'
-        },
-        rollupOptions: {
-            external: ['commander']
-        },
-        outDir: 'dist',
-        sourcemap: true,
-        ssr: true,
-    }
-})
+  plugins: [
+    dts({
+      outDir: "dist/types",
+    }),
+  ],
+  build: {
+    target: "node22",
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: () => "index.js",
+    },
+    rollupOptions: {
+      external: ["commander"],
+    },
+    outDir: "dist",
+    sourcemap: true,
+    ssr: true,
+  },
+});
--- packages/cli/vitetest.config.ts
+++ packages/cli/vitetest.config.ts
@@ -1,38 +1,37 @@
-import { defineConfig } from 'vitest/config'
-import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
-
+import { defineConfig } from "vitest/config";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-    plugins: [
-        dts({
-            outDir: 'dist/types'
-        })
-    ],
-    optimizeDeps: {
-        exclude: ['fsevents']
+  plugins: [
+    dts({
+      outDir: "dist/types",
+    }),
+  ],
+  optimizeDeps: {
+    exclude: ["fsevents"],
+  },
+  build: {
+    target: "es2020",
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: (format, entry) => `${entry}.${format}.js`,
     },
-    build: {
-        target: 'es2020',
-        lib: {
-            entry: resolve(__dirname, 'src/index.ts'),
-            formats: ['es'],
-            fileName: (format, entry) => `${entry}.${format}.js`
-        },
-        outDir: 'dist',
-        rollupOptions: {
-            external: [
-                'vite',
-                /^vite\/.*/,  // Externalize all Vite subpaths
-                'fsevents',
-                /^node\:.*/,
-                'fetch-retry' 
-            ],
-            output: {
-                // preserveModules: true,
-                entryFileNames: '[name].js',
-                dir: 'dist'
-            }
-        }
-    }
-})
+    outDir: "dist",
+    rollupOptions: {
+      external: [
+        "vite",
+        /^vite\/.*/, // Externalize all Vite subpaths
+        "fsevents",
+        /^node\:.*/,
+        "fetch-retry",
+      ],
+      output: {
+        // preserveModules: true,
+        entryFileNames: "[name].js",
+        dir: "dist",
+      },
+    },
+  },
+});
