583104b546ad3a6ccb61d4f8b32a7e1da1dafbc9
Islam Shehata <islam@axiom.co>
2025-06-11 12:08:52 +0400

    remove projects

--- packages/sdk/src/types.ts
+++ packages/sdk/src/types.ts
@@ -1,18 +1,23 @@
 export type PromptInput = {
     name: string
+    slug: string // e.g: 'my-prompt'
     messages: {role: string, content: string}[]
-    model: string
-    temperature: number
+    model: {
+        id: string // e.g: 'gpt-4'
+        provider: string // e.g: 'openai'
+        params: {
+            temperature: number, // e.g: 0.5
+            max_tokens?: number // e.g: 150
+        }
+    }
+    /* map of key-value pairs that are passed to the prompt */
+    arguments: Record<string, any>
 }
 
 export type Prompt = PromptInput & {
     id: string
     environment: 'production' | 'staging' | 'development' | null
     version: string;
-}
-
-export type Project = {
-    name: string
 }
 
 export type LibraryInput = {
