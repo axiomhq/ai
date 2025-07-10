0b8952f095712f2b88abfd7556bff566c6716dd9
Islam Shehata <islam@axiom.co>
2025-07-10 15:45:03 +0300

    fix(test): coverage and missing deps (#4)
    * fix(test): coverage and missing deps
    
    * fix(lint): run prettier format

--- examples/nextjs-otel-example/src/app/page.tsx
+++ examples/nextjs-otel-example/src/app/page.tsx
@@ -1,30 +1,27 @@
-import { generateText } from "ai";
-import { geminiFlash } from "@/shared/gemini";
-import { withSpan } from "@axiomhq/ai";
+import { generateText } from 'ai';
+import { geminiFlash } from '@/shared/gemini';
+import { withSpan } from '@axiomhq/ai';
 
 export default async function Page() {
   const userId = 123;
-  const res = await withSpan(
-    { workflow: "help_user", task: "get_capital" },
-    (span) => {
-      // you have access to the span in this callback
-      span.setAttribute('user_id', userId);
+  const res = await withSpan({ workflow: 'help_user', task: 'get_capital' }, (span) => {
+    // you have access to the span in this callback
+    span.setAttribute('user_id', userId);
 
-      return generateText({
-        model: geminiFlash,
-        messages: [
-          {
-            role: "system",
-            content: "You are a helpful ai that answers questions.",
-          },
-          {
-            role: "user",
-            content: "What is the capital of Spain?",
-          },
-        ],
-      });
-    }
-  );
+    return generateText({
+      model: geminiFlash,
+      messages: [
+        {
+          role: 'system',
+          content: 'You are a helpful ai that answers questions.',
+        },
+        {
+          role: 'user',
+          content: 'What is the capital of Spain?',
+        },
+      ],
+    });
+  });
 
   return <p>{res.text}</p>;
 }
