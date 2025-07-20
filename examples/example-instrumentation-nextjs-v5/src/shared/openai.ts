9f35d5cfc32c95b147a88a001d05d5ade38621b5
c-ehrlich <ehrlich.christopher@gmail.com>
2025-07-20 17:10:46 +0700

    update v5 example

---
+++ examples/example-instrumentation-nextjs-v5/src/shared/openai.ts
@@ -0,0 +1,8 @@
+import { createOpenAI } from '@ai-sdk/openai';
+import { wrapAISDKModel } from '@axiomhq/ai';
+
+const openai = createOpenAI({
+  apiKey: process.env.OPENAI_API_KEY!,
+});
+
+export const gpt4oMini = wrapAISDKModel(openai('gpt-4o-mini'));
