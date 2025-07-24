a5ac29b272aa7ad2d96bd02e8bc3a8b8c266c79d
c-ehrlich <ehrlich.christopher@gmail.com>
2025-07-24 10:51:54 +0700

    feat: warn if no tracer found, add jsdoc

--- packages/ai/src/otel/initAxiomAI.ts
+++ packages/ai/src/otel/initAxiomAI.ts
@@ -1,6 +1,14 @@
 import type { Tracer } from '@opentelemetry/api';
 import { AxiomAIResources } from './shared';
 
+/**
+ * Register this in your `instrumentation.ts` to set up @axiomhq/ai.
+ * This function registers the tracer to enable Context Propagation and Instrumentation Scope.
+ * If you do not register a tracer, some spans may not be linked properly.
+ *
+ * @param config
+ * @param config.tracer - The tracer that you are using in your application.
+ */
 export function initAxiomAI(config: { tracer: Tracer }) {
   AxiomAIResources.getInstance().init(config);
 }
