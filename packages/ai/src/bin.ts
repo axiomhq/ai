58691439a10a619fce898707e1bb68b0d448e914
Gabriel De Andrade <gabrieldeandradeleal@gmail.com>
2025-07-29 21:54:53 -0400

    fix: non-existent imports

--- packages/ai/src/bin.ts
+++ packages/ai/src/bin.ts
@@ -10,8 +10,6 @@ loadEnvConfig(process.cwd());
 import { Command } from 'commander';
 import { loadPushCommand } from './commands/push';
 import { loadPullCommand } from './commands/pull';
-import { loadDeleteCommand } from './commands/delete';
-import { loadListCommand } from './commands/list';
 import { loadRunCommand } from './commands/run';
 
 const program = new Command();
@@ -21,10 +19,8 @@ program
   .description("Axiom's CLI to manage your objects and run evals")
   .version('1.0.0');
 
-loadListCommand(program);
 loadPushCommand(program);
 loadPullCommand(program);
-loadDeleteCommand(program);
 loadRunCommand(program);
 
 program.parse();
