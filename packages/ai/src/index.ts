17e1104429c6a61fb9bef632476bb1ddb532500e
Islam Shehata <islam@axiom.co>
2025-07-31 12:34:01 +0300

    export evals in index

--- packages/ai/src/index.ts
+++ packages/ai/src/index.ts
@@ -28,3 +28,15 @@ export type {
   ParsedMessagesArray as ParsedMessagesArray_experimental,
   ParsedPrompt as ParsedPrompt_experimental,
 } from './types/metadata';
+
+
+// Eval functionality - marked as EXPERIMENTAL as these APIs might change
+export { Eval as Eval_experimental } from './evals/eval';
+export type {
+  EvalTask as EvalTask_experimental,
+  EvalParams as EvalParams_experimental,
+  Score as Score_experimental,
+  EvalReport as EvalReport_experimental,
+} from './evals/eval';
+
+export { AxiomReporter as AxiomReporter_experimental } from './evals/reporter';
