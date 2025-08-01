890e632887f02b86fe5e37ed6ffe0bba4b177675
Gabriel De Andrade <gabrieldeandradeleal@gmail.com>
2025-08-01 03:24:04 -0400

    feat: add example middleware with prompt integration

---
+++ examples/example-middleware-prompt/intrumentation.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import { trace } from '@opentelemetry/api';
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
+import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { Resource } from '@opentelemetry/resources';
+import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { initAxiomAI } from '@axiomhq/ai';
+
+export function initializeTelemetry() {
+  // Initialize telemetry
+  const exporter = new OTLPTraceExporter({
+    url: `${process.env['AXIOM_URL']}/v1/traces`,
+    headers: {
+      Authorization: `Bearer ${process.env['AXIOM_TOKEN']}`,
+      'X-Axiom-Dataset': process.env['AXIOM_DATASET']!,
+    },
+  });
+
+  const consoleExporter = new ConsoleSpanExporter();
+
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [ATTR_SERVICE_NAME]: 'middleware-example',
+    }),
+    spanProcessors: [new SimpleSpanProcessor(exporter), new SimpleSpanProcessor(consoleExporter)],
+  });
+
+  provider.register();
+  const tracer = trace.getTracer('middleware-example');
+
+  initAxiomAI({ tracer });
+}
