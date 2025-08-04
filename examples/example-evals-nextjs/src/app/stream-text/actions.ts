38acc7387521b8581fe470cc4c3a27b4efc78066
Islam Shehata <islam@axiom.co>
2025-08-05 02:01:04 +0300

    feat(evals): register evals at Axiom

---
+++ examples/example-evals-nextjs/src/app/stream-text/actions.ts
@@ -0,0 +1,27 @@
+'use server';
+
+import { streamText } from 'ai';
+import { createStreamableValue } from '@ai-sdk/rsc';
+import { gpt4oMini } from '@/shared/openai';
+import { withSpan } from '@axiomhq/ai';
+
+export async function generateStreamingText(input: string) {
+  const stream = createStreamableValue('');
+
+  (async () => {
+    withSpan({ capability: 'example', step: 'stream' }, async () => {
+      const { textStream } = streamText({
+        model: gpt4oMini,
+        prompt: input,
+      });
+
+      for await (const delta of textStream) {
+        stream.update(delta);
+      }
+
+      stream.done();
+    });
+  })();
+
+  return { output: stream.value };
+}
