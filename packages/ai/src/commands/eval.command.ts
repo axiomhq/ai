325c16c9e52a2596eef35642d7c679fafa6910de
Islam Shehata <islam@axiom.co>
2025-08-13 17:59:13 +0300

    chore(cli): rename axiom run to exiom eval

--- packages/ai/src/commands/eval.command.ts
+++ packages/ai/src/commands/eval.command.ts
@@ -3,9 +3,9 @@ import { runVitest } from '../evals/run-vitest';
 
 export const loadRunCommand = (program: Command) => {
   return program.addCommand(
-    new Command('run')
+    new Command('eval')
       .description('run evals locally')
-      .argument('<path>', 'Path to an eval test file, should be in the form of name.eval.ts')
+      .argument('<path>', 'Path to an eval test file, should be in the form of *.eval.ts')
       .action(async (file: string) => {
         if (!process.env.AXIOM_URL || !process.env.AXIOM_TOKEN || !process.env.AXIOM_DATASET) {
           throw new Error('AXIOM_URL, AXIOM_TOKEN, and AXIOM_DATASET must be set');
