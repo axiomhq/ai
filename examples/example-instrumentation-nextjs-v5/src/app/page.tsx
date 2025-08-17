b0c8aafecaccc4a269afe886646d0aed68dfc23c
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2025-08-17 19:34:10 +0200

    feat: better stream handling (#57)
    * feat: better stream handling
    
    * type fix

--- examples/example-instrumentation-nextjs-v5/src/app/page.tsx
+++ examples/example-instrumentation-nextjs-v5/src/app/page.tsx
@@ -10,7 +10,10 @@ export default function Page() {
           <Link href="/generate-text">Generate Text Example</Link>
         </li>
         <li>
-          <Link href="/stream-text">Stream Text Example</Link>
+          <Link href="/stream-text-ai-sdk-react">Stream Text Example (@ai-sdk/react)</Link>
+        </li>
+        <li>
+          <Link href="/stream-text-ai-sdk-rsc">Stream Text Example (@ai-sdk/rsc, legacy)</Link>
         </li>
       </ul>
     </div>
