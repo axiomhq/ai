b602aa6758e7c3d675b4e595fcbc747e1c45ca3d
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2025-09-17 19:59:50 +0700

    this is fine

--- examples/example-evals-nextjs/vitest.config.ts
+++ examples/example-evals-nextjs/vitest.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path';
 
-// TODO: BEFORE MERGE - discuss with @islam about how to best handle vitest configs (we dont have `defineConfig` here)
+// TODO: discuss with @islam about how to best handle vitest configs (we dont have `defineConfig` here unless we peer dep vitest)
 
 export default {
   resolve: {
