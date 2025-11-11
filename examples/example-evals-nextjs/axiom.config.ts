bd47f639bf3ac0a6c58c517cdb11b460495f0d13
Gabriel De Andrade <gabrieldeandradeleal@gmail.com>
2025-11-11 13:08:10 -0400

    refactor(evals): remove temporary hack for resources URL in config

--- examples/example-evals-nextjs/axiom.config.ts
+++ examples/example-evals-nextjs/axiom.config.ts
@@ -3,9 +3,6 @@ import { setupAppInstrumentation } from './src/instrumentation.node';
 
 export default defineConfig({
   eval: {
-    // @ts-expect-error - this is a temporary hack to allow us to use the resources url
-    __overrideEndpointUrl: process.env.AXIOM_RESOURCES_URL,
-
     include: ['**/*.eval.{ts,js,mts,mjs,cts,cjs}'],
     exclude: [],
 
